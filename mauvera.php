13b21a3c4fa506c1b21312de36a01899cd43b8f8
Omonzejele Precious <crispusomonze@gmail.com>
2018-06-07 01:14:23 +0100

    enqeueing properly

--- mauvera.php
+++ mauvera.php
@@ -18,3 +18,15 @@ if ( !function_exists( 'add_action' ) ) {
 	echo 'Yo!  I\'m just a plugin, not much I can do when called directly. I love being moved along with the wp family';
 	exit;
 }
+//version
+$mauv_pk_version = '1.0';
+//start enqeueing
+if(!function_exists('mauv_pk_js_enqueue')){
+	function mauv_pk_js_script() {
+		//p_enqueue_script('NameMySccript','path/to/MyScript','dependencies_MyScript', 'VersionMyScript', 'InfooterTrueorFalse');
+		wp_register_script('mauv_pk_js-buy-ticket','https://mauvera.com',null,$mauv_pk_version,true);
+		wp_enqueue_script( 'mauv_pk_js-buy-ticket');
+	}
+}
+//just incase we encounter a wretched theme that doesn't have wp_footer(), we should hook up with print scripts
+add_action( 'wp_print_scripts', 'mauv_pk_js_script' );
