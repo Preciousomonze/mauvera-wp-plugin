6efeee7087858f35922e6a7746f80011c5b6e871
Omonzejele Precious <crispusomonze@gmail.com>
2018-06-10 22:16:52 +0100

    cleansing shortcode file

---
+++ inc/admin/shortcodes.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The file handling the measurement feature in the order page in dashboard
+ * I'm talking like this or typing like this cause i'm tired, no vex :(
+ * you shaa understand what i mean.
+ * 
+ * @author Precious Omonzejele <omonze@peepsipi.com>
+ */
+if(!defined('ABSPATH')){
+	exit();
+}
